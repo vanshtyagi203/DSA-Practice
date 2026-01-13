21986f56173c87f6ebbc04e26e64bc686f36460c
Vansh Tyagi <vanshtyagi203@gmail.com>
2026-01-13 20:21:06 +0530

    First basic code of Strings

---
+++ String/basic.cpp
@@ -0,0 +1,18 @@
+//STRING=strings are classes
+//DYNAMIC MEM ALLOCATION 
+//MANY INBUILT FUNCTIONS
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string str;
+    getline(cin,str);
+     cout<<str<<endl;
+
+    return 0;
+}
+
+//
