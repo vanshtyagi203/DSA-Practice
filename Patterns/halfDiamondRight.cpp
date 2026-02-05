9eb68149c01e9e4e4734a8d4c8ca68bb5063c162
Vansh Tyagi <vanshtyagi203@gmail.com>
2026-02-05 16:18:20 +0530

    Half Diamond

---
+++ Patterns/halfDiamondRight.cpp
@@ -0,0 +1,32 @@
+/*
+
+    *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+    
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+     for(int i=1;i<=2*n-1;i++){
+        int star=i;
+        if(i>n){
+          star=2*n-i;
+        }
+        for(int j=1;j<=star;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+     }
+     return 0;
+}
